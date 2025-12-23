a0fb41889db93d8dedad3997e4f90ededcd3c883
keagan <developerkeagan@gmail.com>
2025-12-23 09:55:00 +0100

    Initial commit: ecommercewalker farms

--- utils/email.js
+++ utils/email.js
@@ -1,14 +1,17 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
+    const port = Number(process.env.SMTP_PORT) || 587;
+
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: false, // true for 465, false for other ports
+        port: port,
+        secure: port === 465, // true for 465, false for other ports
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
         },
+        connectionTimeout: 10000, // 10 seconds
     });
 
     const message = {
